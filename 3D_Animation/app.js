4017d0cd717f0bff9251bb1e144191bcd25227e2
Song Jaewoo <xoals19958@gmail.com>
2021-06-07 00:24:55 +0900

    06/07~06/07
    3D Animation and full-webpage with dynamic animations

---
+++ 3D_Animation/app.js
@@ -0,0 +1,39 @@
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes')
+
+//moving animation
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerWidth / 2 - e.pageY) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+//animateIn
+container.addEventListener('mouseenter', e => {
+    card.style.transition = 'none';
+
+    title.style.transform = 'translateZ(150px)';
+    sneaker.style.transform = 'translateZ(200px) rotateZ(-10deg)';
+    description.style.transform = 'translateZ(125px)';
+    sizes.style.transform = 'translateZ(100px)';
+    purchase.style.transform = 'translateZ(75px)';
+})
+
+//animateOut
+container.addEventListener('mouseleave', e => {
+    card.style.transition = 'all 0.5s ease-in';
+    card.style.transform = `rotate(0deg) rotateX(0deg)`
+
+    title.style.transform = 'translateZ(0px)';
+    sneaker.style.transform = 'translateZ(0px) rotateZ(0deg)';
+    description.style.transform = 'translateZ(0px)';
+    sizes.style.transform = 'translateZ(0px)';
+    purchase.style.transform = 'translateZ(0px)';
+})
+
